3cca1d37666160671def73d571655daa099db97f
Adam Cigánek <adam.ciganek@gmail.com>
2021-10-18 14:54:20 +0200

    Improve the integration test

--- bip_dht/tests/tests.rs
+++ bip_dht/tests/tests.rs
@@ -28,11 +28,14 @@ fn basic() {
     // make B aware that A has the infohash.
     node_a.search(the_info_hash, true);
 
+    let mut lookup_completed = false;
+
     for event in events_a.iter() {
         match event {
             DhtEvent::BootstrapCompleted => (),
             DhtEvent::LookupCompleted(info_hash) => {
                 assert_eq!(info_hash, the_info_hash);
+                lookup_completed = true;
                 break;
             }
             event @ (DhtEvent::PeerFound(..) | DhtEvent::ShuttingDown(_)) => {
@@ -41,23 +44,31 @@ fn basic() {
         }
     }
 
+    assert!(lookup_completed);
+
     // Now perform the lookup by B. It should find A.
     node_b.search(the_info_hash, false);
 
-    let mut found = false;
+    let mut peer_found = false;
+    let mut lookup_completed = false;
+
     for event in events_b.iter() {
         match event {
             DhtEvent::BootstrapCompleted => (),
-            DhtEvent::LookupCompleted(info_hash) => assert_eq!(info_hash, the_info_hash),
+            DhtEvent::LookupCompleted(info_hash) => {
+                assert_eq!(info_hash, the_info_hash);
+                lookup_completed = true;
+                break;
+            }
             DhtEvent::PeerFound(info_hash, addr) => {
                 assert_eq!(info_hash, the_info_hash);
                 assert_eq!(addr, a_addr);
-                found = true;
-                break;
+                peer_found = true;
             }
             event @ DhtEvent::ShuttingDown(_) => panic!("unexpected event {:?}", event),
         }
     }
 
-    assert!(found);
+    assert!(peer_found);
+    assert!(lookup_completed);
 }
